05995b7fa5baae2cad250cb5a5a4290d08ff6db7
ks1f <168671615+ks1f@users.noreply.github.com>
2025-01-16 22:15:12 +0100

    Add files via upload

---
+++ hw1.1_js/index.js
@@ -0,0 +1,5 @@
+const firstNumber=Number (prompt('Please, type any number'));
+const secondNumber=  Number (prompt('Please, enter any number again'));
+const result= firstNumber + secondNumber;
+
+alert(result)
---
+++ hw1_js/index.js
@@ -0,0 +1,9 @@
+let username = prompt('What`s your name?');
+// let firstNumber =prompt('Please, enter any number')
+// let secondNumber=prompt('Please, enter any number again')
+// const result= (`${firstNumber} + ${secondNumber} = `)
+
+
+alert(`Hi, ${username}!, How are you?`);
+
+
