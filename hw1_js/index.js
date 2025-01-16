04fb305c241389d67f5619ef99fde66a509392e0
ks1f <168671615+ks1f@users.noreply.github.com>
2025-01-16 22:16:28 +0100

    Update index.js

--- hw1_js/index.js
+++ hw1_js/index.js
@@ -1,9 +1,3 @@
 let username = prompt('What`s your name?');
-// let firstNumber =prompt('Please, enter any number')
-// let secondNumber=prompt('Please, enter any number again')
-// const result= (`${firstNumber} + ${secondNumber} = `)
-
 
 alert(`Hi, ${username}!, How are you?`);
-
-
